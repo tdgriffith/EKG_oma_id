5f70a9c2dc65ae7ddf2b3618e276bbea8ebd3233
tdgriffith <griffith@iastat.edu>
2021-06-16 08:35:37 -0600

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Modal decomposition of EKG data for ML classification',
+    author='Kevin Fuentes, Tristan Griffith',
+    license='MIT',
+)
